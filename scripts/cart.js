958a5329a46cc5bf135d7bc3b60671bd6b2229b4
Sebastian Luna <sebas1997321@gmail.com>
2022-05-21 15:27:53 -0500

    navbar y footer modularizados

---
+++ scripts/cart.js
@@ -0,0 +1,11 @@
+import navbar from "../modules/navbar.js";
+import footer from "../modules/footer.js";
+
+
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const nav = document.querySelector("nav#navbar");
+    navbar(nav);
+    const foot = document.getElementById("containerFooter");
+    footer(foot);
+})
--- scripts/prenda.js
+++ scripts/prenda.js
@@ -1,5 +1,18 @@
+import navbar from "../modules/navbar.js";
+import footer from "../modules/footer.js";
+
+
+
 import GetData from '../helpers/getData.js';
 import showPrend from '../modules/showPrend.js';
+
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const nav = document.querySelector("nav#navbar");
+    navbar(nav);
+    const foot = document.getElementById("containerFooter");
+    footer(foot);
+})
 
 const container = document.querySelector('#containerPrend');
 container.addEventListener('DOMContentLoaded', async () => {
