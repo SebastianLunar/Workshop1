57077a837eba13e815aa51cc5dd47e579e562f7a
Santiago <santiagovalencialeon@gmail.com>
2022-05-22 16:54:20 -0500

    agregado funcion de clikc en imagenes

--- scripts/prenda.js
+++ scripts/prenda.js
@@ -4,7 +4,8 @@ import navbar from "../modules/navbar.js";
 import footer from "../modules/footer.js";
 import GetData from '../helpers/getData.js';
 import printSelectCard from '../modules/printSelectedCard.js';
-
+import cuadro1 from '../modules/printCuadro1.js';
+import cuadro2 from '../modules/printCuadro2.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
     const container = document.querySelector('#main-container');
@@ -24,5 +25,20 @@ document.addEventListener('click', ({target})=>{
         let id = target.id
         localStorage.setItem("selected", id)
         window.location.href = "../pages/prenda.html"
+    }
+})
+
+document.addEventListener('click', async (e)=>{
+    const container = document.querySelector('#main-container');
+    let id = localStorage.getItem("selected");
+    const { data } = await GetData(productos+'/'+id);
+    if (e.target.id === "cuadro-1") {
+    cuadro1(container, data);
+    }
+    if (e.target.id === "cuadro-2") {
+    cuadro2(container, data);
+    }
+    if (e.target.id === "cuadro-3") {
+    printSelectCard(container, data);
     }
 })
