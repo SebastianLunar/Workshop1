141b2071c9b8e968ba080f745f5bd5d384dd0d52
Sebastian Luna <sebas1997321@gmail.com>
2022-05-22 17:17:55 -0500

    compilado

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,5 @@
-import GetData from "../helpers/getData.js"
 import navbar from "../modules/navbar.js";
 import footer from "../modules/footer.js";
-
-
-const container = document.getElementById('containerCards');
 
 document.addEventListener('DOMContentLoaded', async () => {
     const nav = document.querySelector("nav#navbar");
